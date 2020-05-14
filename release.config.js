5076137aa5a14d82d46fc3bbe205caf339e8bde1
aaronlam <aaronlam1618@gmail.com>
2020-05-14 16:17:51 -0700

    fix: prettier fix

--- release.config.js
+++ release.config.js
@@ -2,8 +2,8 @@ module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/aaron-lam/github-actions-example",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
   ]
 };
